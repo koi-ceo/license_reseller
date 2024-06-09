f8ed754a33f700d4b2f287aa26f39ab0d90edc29
koi-ceo <ceo.koicorp@gmail.com>
2024-06-09 15:29:01 +0900

    [UPDATE] datepicker 완료

--- plugin/jquery-ui/datepicker.php
+++ plugin/jquery-ui/datepicker.php
@@ -9,5 +9,33 @@ if (!defined('_KOI_')) exit; // 개별 페이지 접근 불가
 
 ?>
 
-<link type="text/css" href="<?= KOI_PLUGIN_URL ?>/jquery-ui/datepicker.css">
-<script src="<?= KOI_PLUGIN_URL ?>/jquery-ui/datepicker.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/bulma-calendar/dist/css/bulma-calendar.min.css">
+<script src="https://unpkg.com/bulma-calendar/dist/js/bulma-calendar.min.js"></script>
+
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        let calendars_datetime = bulmaCalendar.attach('[type="datetime"]', {
+            dateFormat   : 'yyyy-MM-dd',
+            showButtons  : true,
+            startDate    : new Date(),
+            lang         : 'ko',
+            validateLabel: '확인',
+            todayLabel   : '오늘',
+            clearLabel   : '초기화',
+            cancelLabel  : '취소'
+        });
+
+        let calendars_date = bulmaCalendar.attach('[type="date"]', {
+            dateFormat   : 'yyyy-MM-dd',
+            showButtons  : true,
+            startDate    : new Date(),
+            lang         : 'ko',
+            validateLabel: '확인',
+            todayLabel   : '오늘',
+            clearLabel   : '초기화',
+            cancelLabel  : '취소'
+        });
+    });
+
+</script>
